8bbe696d45aecf9856178e4f54d82452867c0660
mazen41 <mazen41@users.noreply.github.com>
2026-02-24 14:03:22 +0100

    Add Odoo.sh configuration file for auto-install

---
+++ .odoo.sh
@@ -0,0 +1,9 @@
+# Odoo.sh Configuration File
+# This file tells Odoo.sh how to handle this repository
+
+[options]
+# Odoo version
+version = 17.0
+
+# Install these modules on database creation
+install = engineering_core,engineering_quotation,engineering_packages,engineering_project,engineering_contracts,engineering_documents,engineering_reports
