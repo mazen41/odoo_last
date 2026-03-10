ef49b39bbca822230199eb1455c0316ce3e9ca97
mazen41 <161659553+mazen41@users.noreply.github.com>
2026-03-10 22:48:52 +0200

    Update __manifest__.py

--- engineering_project/__manifest__.py
+++ engineering_project/__manifest__.py
@@ -13,8 +13,13 @@
         'reports/initial_design_report.xml', # <--- ADD THIS
         'views/project_project_views.xml',
         'data/project_task_type_data.xml',
-
+    
     ],
+       'assets': {
+        'web.assets_backend': [
+            'engineering_project/static/src/css/task_state.css',
+        ],
+    },
     'license': 'LGPL-3',
     'installable': True,
     'application': True,
