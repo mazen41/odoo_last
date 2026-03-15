f399da6271197cdc0eab30182f2e18e73f1ae4e8
mazen41 <mazehossny@gmail.com>
2026-03-15 20:15:23 +0200

    message

--- engineering_core/models/models.py
+++ engineering_core/models/models.py
@@ -18,7 +18,7 @@ class KuwaitRegion(models.Model):
 
 
 # ==============================================================================
-#  RES PARTNER
+#  RES PARTNER (Customer Profile)
 # ==============================================================================
 class ResPartner(models.Model):
     _inherit = 'res.partner'
@@ -31,7 +31,6 @@ class ResPartner(models.Model):
     street_no = fields.Char(string="الضاحيه")
     area = fields.Char(string="مساحة الارض (Area)")
     
-    # Updated to Many2one
     governorate_id = fields.Many2one('kuwait.governorate', string="المحافظة")
     region_id = fields.Many2one('kuwait.region', string="المنطقة (Region)")
 
@@ -39,31 +38,8 @@ class ResPartner(models.Model):
 # ==============================================================================
 #  CRM LEAD
 # ==============================================================================
-class CrmLead(models.Model):
-    _inherit = 'crm.lead'
-
-    building_type = fields.Selection([('residential', 'سكن خاص'), ('investment', 'استثماري'), ('commercial', 'تجاري'), ('industrial', 'صناعي'), ('cooperative', 'جمعيات وتعاونيات'), ('mosque', 'مساجد'), ('hangar', 'مخازن / شبرات'), ('farm', 'مزارع')], string="نوع العقار")
-    service_type = fields.Selection([('new_construction', 'بناء جديد'), ('demolition', 'هدم'), ('modification', 'تعديل'), ('addition', 'اضافة'), ('addition_modification', 'تعديل واضافة'), ('supervision_only', 'إشراف هندسي فقط'), ('renovation', 'ترميم'), ('internal_partitions', 'قواطع داخلية'), ('shades_garden', 'مظلات / حدائق')], string="نوع الخدمة")
-    plot_no = fields.Char(string="رقم القسيمة (Plot)")
-    block_no = fields.Char(string="القطعة (Block)")
-    street_no = fields.Char(string="الضاحيه")
-    area = fields.Char(string="مساحة الارض (Area)")
-    
-    # Updated to Many2one
-    governorate_id = fields.Many2one('kuwait.governorate', string="المحافظة")
-    region_id = fields.Many2one('kuwait.region', string="المنطقة (Region)")
-    
-    def _prepare_sale_order_values(self, partner, company, access_token):
-        values = super()._prepare_sale_order_values(partner, company, access_token)
-        values['building_type'] = self.building_type
-        values['service_type'] = self.service_type
-        values['plot_no'] = self.plot_no
-        values['block_no'] = self.block_no
-        values['street_no'] = self.street_no
-        values['area'] = self.area
-        values['governorate_id'] = self.governorate_id.id
-        values['region_id'] = self.region_id.id
-        return values
+# All custom fields and data-passing functions have been removed. 
+# Odoo will just use the standard Name, Email, Phone, etc.
 
 
 # ==============================================================================
@@ -80,6 +56,18 @@ class SaleOrder(models.Model):
     street_no = fields.Char(string="الضاحيه", store=True)
     area = fields.Char(string="مساحة الارض", store=True)
     
-    # Updated to Many2one
     governorate_id = fields.Many2one('kuwait.governorate', string="المحافظة", store=True)
     region_id = fields.Many2one('kuwait.region', string="المنطقة (Region)", store=True)
+
+    # Automatically pull these details from the Customer when creating a Quotation
+    @api.onchange('partner_id')
+    def _onchange_partner_id_engineering_fields(self):
+        if self.partner_id:
+            self.building_type = self.partner_id.building_type
+            self.service_type = self.partner_id.service_type
+            self.plot_no = self.partner_id.plot_no
+            self.block_no = self.partner_id.block_no
+            self.street_no = self.partner_id.street_no
+            self.area = self.partner_id.area
+            self.governorate_id = self.partner_id.governorate_id
+            self.region_id = self.partner_id.region_id
