ccdd642714d2f1abddeda167791e4e5693fa9178
mazen41 <161659553+mazen41@users.noreply.github.com>
2026-03-20 21:39:17 -0700

    Update __init__.py

--- engineering_commitments/models/__init__.py
+++ engineering_commitments/models/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 from . import sign_template
-from . import project_taks # This file contains ProjectProject and ProjectTask extensions
+from . import project_task
 from . import engineering_task_commitment
 from . import engineering_project_commitment
